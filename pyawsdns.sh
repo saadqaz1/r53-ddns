dedec36e88509863a58fa981d6d7617e3a4107ac
sq <saadqazi23@gmail.com>
2022-11-27 19:39:42 -0500

    updated cron shell script

--- pyawsdns.sh
+++ pyawsdns.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'starting pyawsdns script'
-source /etc/opt/py-aws-r53-dns/env-py/bin/activate
-python3 /etc/opt/py-aws-r53-dns/awsdns.py
+source ./env-py/bin/activate
+python3 ./awsdns.py
